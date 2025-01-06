f510c58a2b261c9b4c16af1caf272b38b3444e63
jaroslavhrazdera <jarda.hrazdera@gmail.com>
2025-01-06 15:20:47 +0100

    Integrated Set Temperature form into Tank Dashboard with dynamic functionality for target temperature updates.

--- FermentationController/settings.py
+++ FermentationController/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'dashboard/static',
 ]
 
+# Directory where static files will be collected during collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
